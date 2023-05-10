ef9dcaebffd92920b9849ccb4316727ad0b461c4
w2mmw <w2mmweller@gmail.com>
2023-05-10 19:01:58 -0400

    Updated 5-19-23

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+let isItMetric = true;
+let classConvertHeading = document.QuerySelector(".convert-heading");
+let classCovertText = document.QuerySelector(".convert-text");
+let classbtnconvert = document.QuerySelector(".btn-convert");
+alert(classbtnconvert);
+input("WTF?");
+let classbtnchange = document.QuerySelector(".btn-change");
+let classlengthcaption = document.QuerySelector(".length-caption");
+let classlengthtext = document.QuerySelector(".length-text");
+let classvolumecaption = document.QuerySelector(".volume-caption");
+let classvolumetext = document.QuerySelector(".volume-text");
+let classmasscaption = document.QuerySelector(".mass-caption");
+let classmasstext = document.QuerySelector(".mass-text");
+
+classbtnconvert.addEventListener("click", function(){
+    alert("button pressed");
+})
